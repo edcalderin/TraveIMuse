404f00a824b92664a9e49af8ee4bd8505fe10412
Erick <edcm.erick@gmail.com>
2025-04-10 15:00:01 -0500

    ✨ feat: Rename directory name

---
+++ src/itinerary/__init__.py
@@ -0,0 +1,2 @@
+from src.itinerary.itinerary_template import ItineraryTemplate as ItineraryTemplate
+from src.itinerary.travel_agent import TravelAgent as TravelAgent
--- src/llm/__init__.py
+++
@@ -1,2 +0,0 @@
-from src.llm.itinerary_template import ItineraryTemplate as ItineraryTemplate
-from src.llm.travel_agent import TravelAgent as TravelAgent
